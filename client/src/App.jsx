7e1b5c604864189442355dffa7b809872f4acd3c
Subodh <subodhsurwade04@gmail.com>
2024-05-03 22:19:44 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Topic from "./pages/Topic";
 import Question from "./pages/Question";
@@ -8,15 +8,13 @@ import Result from "./pages/Result";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home showLogin={false} />} />
-          <Route path="/login" element={<Home showLogin />} />
-          <Route path="/topic" element={<Topic />} />
-          <Route path="/questions/:category" element={<Question />} />
-          <Route path="/result/:points" element={<Result />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home showLogin={false} />} />
+        <Route path="/login" element={<Home showLogin />} />
+        <Route path="/topic" element={<Topic />} />
+        <Route path="/questions/:category" element={<Question />} />
+        <Route path="/result/:points" element={<Result />} />
+      </Routes>
     </>
   );
 }
