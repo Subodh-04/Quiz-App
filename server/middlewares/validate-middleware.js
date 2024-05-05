1111d52c532c207be5db6a0d770f78c80b219ead
Subodh <subodhsurwade04@gmail.com>
2024-05-05 21:42:52 +0530

    removed errors at server side

--- server/middlewares/validate-middleware.js
+++ server/middlewares/validate-middleware.js
@@ -5,14 +5,15 @@ const validate = (schema) => async (req, res, next) => {
     next();
   } catch (err) {
     const status = 422;
-    const message="Fill the input Properly";
-    const extraDetails = err.errors[0].message;
+    const message = "Fill the input Properly";
+    const extraDetails=err.errors[0].message;
+
     const error = {
       status,
       message,
       extraDetails,
     };
-    console.log(error);
+    console.log(error); 
     next(error);
   }
 };
